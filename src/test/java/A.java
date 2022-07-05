ff72404fbdaf490388e43a7e1a378aa717528201
Meena-khan <84698095+Meena-khan@users.noreply.github.com>
2022-07-05 12:37:53 +0530

    iniial commit

--- src/test/java/A.java
+++ src/test/java/A.java
@@ -5,6 +5,15 @@ public class A {
     {
         System.out.println("HI 786");
         System.out.println("HI Bismillah");
+        System.out.println("HI");
+        System.out.println("HI2");
+        System.out.println("HI3");
+
+        System.out.println("HI5");
+
+
+
+
 
     }
 }
